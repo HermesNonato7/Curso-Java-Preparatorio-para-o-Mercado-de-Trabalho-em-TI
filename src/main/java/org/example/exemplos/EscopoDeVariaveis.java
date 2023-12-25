30a79f78f47e92c359a309c784399f9da68d2ee7
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-12-24 23:53:34 -0300

    add EscopoDeVariaveis

---
+++ src/main/java/org/example/exemplos/EscopoDeVariaveis.java
@@ -0,0 +1,27 @@
+package org.example.exemplos;
+
+public class EscopoDeVariaveis {
+    static int a;//nÃ£o precisa ser inicialida por ser uma variÃ¡vel de classe e terÃ¡ o valor padrÃ£o 0
+
+    public static void main (String [] args) {
+      /*
+            Essa classe foi comentada para você conseguir compilar o seu projeto.
+            Remova o comentário acima para visualizar os pontos de erro no código.
+
+           //Os erros abaixo existem de propósito para explicar sobre escopo de variáveis.
+
+        int b;//A variável b precisa ser inicializada por ser uma variável de método.
+
+        if(a == b) {
+            int c;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);//a variável c precisa ser inicializada por ser uma variável de método.
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);//Essa variável só existe dentro do bloco if
+    */
+    }
+}
