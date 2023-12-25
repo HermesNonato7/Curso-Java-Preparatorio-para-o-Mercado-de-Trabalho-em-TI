a4c6fa54ac5f1bb5415a15abbc9f916a6bc69136
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-12-25 00:01:12 -0300

    add exercico 002

--- src/main/java/org/example/exercicios/Exercicio002.java
+++ src/main/java/org/example/exercicios/Exercicio002.java
@@ -16,14 +16,16 @@ public class Exercicio002 {
 
         media = (nota1 + nota2 + nota3 + nota4) / 4
          */
+
+        System.out.println("Digite os valores de 4 notas");
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite um valor: ");
+        System.out.println("Digite o valor da nota 1: ");
         double valor1 = teclado.nextDouble();
-        System.out.println("Digite outro valor: ");
+        System.out.println("Digite o valor da nota 2: ");
         double valor2 = teclado.nextDouble();
-        System.out.println("Digite outro valor: ");
+        System.out.println("Digite o valor da nota 3: ");
         double valor3 = teclado.nextDouble();
-        System.out.println("Digite outro valor: ");
+        System.out.println("Digite o valor da nota 4: ");
         double valor4 = teclado.nextDouble();
         teclado.close();
         double media = (valor1 + valor2 + valor3 + valor4) / 4;
