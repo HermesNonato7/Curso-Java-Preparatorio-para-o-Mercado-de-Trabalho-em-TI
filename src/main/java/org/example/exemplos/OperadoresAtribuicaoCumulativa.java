8b4ae1a237dd271b55964be9fe7da6477c9d2511
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-12-24 22:52:21 -0300

    add codigos OperadoresAtribuicaoCumulativa

---
+++ src/main/java/org/example/exemplos/OperadoresAtribuicaoCumulativa.java
@@ -0,0 +1,51 @@
+package org.example.exemplos;
+
+public class OperadoresAtribuicaoCumulativa {
+
+    public static void main (String [] args) {
+        int a;//declarando uma variável
+        int b = 100;//declarando uma variável e atribuindo um valor
+
+        a = b;//atribuido o valor da variavel b na variável a
+
+        System.out.println(a);
+
+        a = a + b;//somando o conteúdo da variável a com a variável b
+
+        System.out.println(a);
+
+        a += b;////somando o conteúdo da variável a com a variável b
+
+        System.out.println(a);
+
+
+
+       /*
+          A expressão acima vale para os operadores - + * / %
+
+          Exemplos:
+       */
+        a += b;
+        System.out.println(a);
+
+        a -= b;
+        System.out.println(a);
+
+        a *= b;
+        System.out.println(a);
+
+        a /= b;
+        System.out.println(a);
+
+        a %= b;
+        System.out.println(a);
+
+        a = 100;
+        System.out.println("a vale " + a++);
+
+        System.out.println("a vale agora " + a);
+
+        System.out.println("a vale agora " + ++a);
+    }
+
+}
