ebb9c1e0b1d836e960e9e8cab2145a11b0dc63a2
HermesNonato7 <hermes.nonato7@hotmail.com>
2024-01-02 21:37:27 -0300

    Add InterfaceFuncional

---
+++ src/main/java/org/example/exemplos/InterfaceFuncional.java
@@ -0,0 +1,8 @@
+package org.example.exemplos;
+
+/*
+    'Interface funcional' é uma 'interface' que possui um único método abstrato.
+*/
+public interface InterfaceFuncional {
+    void fazAlgo();
+}
